d7cacfab5607c18de5ccad0922b3b5aa3142eade
Joseph Samonte <josephisnt@hotmail.com>
2022-06-15 22:23:11 -0500

    Practicing using router
    left notes at the end of App.js where I left off

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+
+import Home from "./component/Home";
+import About from "./component/About";
+import Messages from "./component/Messages";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
       </header>
+      <div className="menu">
+        <ul>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/messages">Messages</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/about">About</Link>{" "}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
