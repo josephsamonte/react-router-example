9a140a2ed1d5a0079a8fde91af10cdf5719ca679
Joseph Samonte <josephisnt@hotmail.com>
2023-04-06 22:49:57 -0500

    messing with css and className

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,10 +2,10 @@ import "../styles/home.css";
 import React from "react";
 
 const Home = () => (
-  <div className="bg-text">
-    <h2>Home</h2>
+  <div className="bg-text bg-image">
+    <h2 className>Home</h2>
     My Home page!
   </div>
 );
-<div className="bg-image"></div>;
+
 export default Home;
